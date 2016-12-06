7f0d668d9a2091d69a088f39ae5f5b7ea41dd63a
007xiaoxingxing <chanxing9@gmail.com>
2016-12-06 23:47:11 +0800

    add upload

--- js/index.js
+++ js/index.js
@@ -1,3 +1,23 @@
 /**
  * Created by xiaoxing on 2016/12/2.
  */
+$(document).ready(function(){
+
+
+    $(".upload").click(function(){
+
+
+        $('#myModal').modal({
+                keyboard: true
+            })
+    });
+
+    $(".file-select").click(function(){
+
+       // alert(/a/);
+        $("#up-img").click();
+
+    });
+
+
+});
